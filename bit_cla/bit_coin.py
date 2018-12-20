47c24d4aced8227c73ab0e622f14dd6747255b3a
yifpipi <yifpipi@163.com>
2018-12-20 17:58:52 +0800

    aaa

--- bit_cla/bit_coin.py
+++
@@ -1,7 +0,0 @@
-from cryptocmd import CmcScraper
-
-sa  = CmcScraper('BTC')
-
-data = sa.get_dataframe()
-
-data.to_csv('bit.csv',index=False)
--- interface_test/test.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-url = 'http://bayes.tech:8200/api/sentiment/MAN/2018-01-19/2018-11-20'
-#url = 'http://bayes.tech:8200/api/basic/MAN/2018-01-19/2018-11-20'
-#url = 'http://bayes.tech:8200/api/word/MAN/2018-01-19/2018-11-20'
-inf = requests.get(url=url).json()
-print(inf)
-
